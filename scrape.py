1614f7cd00ece1bc3a681a63f33abfba85dbb6e0
Nicolas <github@site7even.de>
2020-04-19 23:53:21 +0200

    refactored to functions

--- scrape.py
+++ scrape.py
@@ -3,48 +3,50 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm, tnrange
 
+def fetch(url, price_el, price_cl, change_el, change_cl):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Chrome/64.0.3282.167 Safari/537.36'
+    }
+
+    result = requests.get(url, headers=headers,verify=True)
+    #print(result)
+
+    soup = BeautifulSoup(result.content, 'html.parser')
+    course = soup.find(price_el, class_=price_cl).getText().strip()
+    change = soup.find(change_el, class_=change_cl).getText().strip()
+    
+    course = xtract(course)
+    
+    return course, change
+
+def xtract(input):
+    if type(input) is not str:
+        return input
+    if "EUR" in input:
+        return float(input.replace("EUR", "").replace(",", ".").strip())
+    else:
+        return input
+
+def report(date, shares, val, newprice):
+    print("\t" + date + "  Anlage:  %5i  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (val, newprice * shares, newprice * shares - val))
+
+
 url = 'https://www.finanzen.net/index/dax-realtime'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
-    'AppleWebKit/537.36 (KHTML, like Gecko)'
-    'Chrome/64.0.3282.167 Safari/537.36'
-}
-result = requests.get(url, headers=headers,verify=True)
-#print(result)
-
-soup = BeautifulSoup(result.content, 'html.parser')
-price = soup.find("div", class_='col-xs-5').getText().strip()
-change = soup.find("div", class_='col-xs-3').getText().strip()
-
-print("DAX: " + price + " " + change)
+points, change = fetch (url, "div", 'col-xs-5', "div", 'col-xs-3')
+print("%s %s %s" % ("DAX:".ljust(15), points, change))
 
 url = "https://www.onvista.de/fonds/UNIDEUTSCHLAND-EUR-ACC-Fonds-DE0009750117"
-result = requests.get(url, headers=headers,verify=True)
-#print(result)
-
-soup = BeautifulSoup(result.content, 'html.parser')
-price = soup.find("span", class_='price').getText().strip()
-change = soup.find("span", class_='performance-pct').getText().strip()
-
-print("UniDeutschland: " + price + " " + change)
-
-unid = float(price.replace("EUR", "").replace(",", ".").strip())
-
-print("\t2020-03-13  Anlage:  7000  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (unid * 47.149, unid * 47.149 - 7000))
-print("\t2020-02-26  Anlage: 10000  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (unid * 47.794, unid * 47.794 - 10000))
+price, change = fetch (url, "span", 'price', "span", 'performance-pct')
+print("%s %8.2f EUR %s" % ("UniDeutschland:".ljust(15), price, change))
+report("2020-03-13", 47.149, 7000, price)
+report("2020-02-26", 47.794, 10000, price)
 
 url = "https://www.onvista.de/fonds/UNIRAK-EUR-DIS-Fonds-DE0008491044"
-result = requests.get(url, headers=headers,verify=True)
-#print(result)
+price, change = fetch (url, "span", 'price', "span", 'performance-pct')
+print("%s %8.2f EUR %s" % ("UniRak:".ljust(15), price, change))
+report("2018-06-29", 82.432, 10000, price)
+report("2018-02-14", 85.657, 10000, price)
 
-soup = BeautifulSoup(result.content, 'html.parser')
-price = soup.find("span", class_='price').getText().strip()
-change = soup.find("span", class_='performance-pct').getText().strip()
-
-print("UniRak: " + price + " " + change)
-
-unir = float(price.replace("EUR", "").replace(",", ".").strip())
-
-print("\t2018-06-29  Anlage: 10000  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (unir * 82.432, unir * 82.432 - 10000))
-print("\t2018-02-14  Anlage: 10000  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (unir * 85.657, unir * 85.657 - 10000))
 
