016a2c012e36b61440b690db66707c45b688d63e
Nicolas <github@site7even.de>
2020-04-22 19:43:29 +0200

    added support for selled shares

--- scrape.py
+++ scrape.py
@@ -29,8 +29,17 @@ def xtract(input):
     else:
         return input
 
-def report(date, shares, val, newprice):
-    print("\t" + date + "  Anlage:  %5i  Akt. Wert: %8.2f EUR  Gewinn: %8.2f EUR" % (val, newprice * shares, newprice * shares - val))
+def report(type, date, shares, val, newprice):
+    if type == "buy":
+        print("\t" + date + "     Kauf: %3.2f/%5i € " % (shares, val),
+        "Akt. Wert: %8.2f € " % (newprice * shares),
+        "Gewinn: %8.2f €" % (newprice * shares - val))
+    else:
+        newlevel = val / newprice
+        newshares = newlevel - shares
+        print("\t" + date + "  Verkauf: %3.2f/%5i € " % (shares, val),
+        "Wiederanlagespiegel: %3.2f Anteile (abzgl. Gebühren) " % (newlevel),
+        "Entwicklung: %3.2f Anteile (%2.2f%%)" % (newshares, 100/shares*newshares))
 
 
 url = 'https://www.finanzen.net/index/dax-realtime'
@@ -40,13 +49,14 @@ print("%s %s %s" % ("DAX:".ljust(15), points, change))
 url = "https://www.onvista.de/fonds/UNIDEUTSCHLAND-EUR-ACC-Fonds-DE0009750117"
 price, change = fetch (url, "span", 'price', "span", 'performance-pct')
 print("%s %8.2f EUR %s" % ("UniDeutschland:".ljust(15), price, change))
-report("2020-03-13", 47.149, 7000, price)
-report("2020-02-26", 47.794, 10000, price)
+report("sell", "2020-04-20", 47.149, 8053.84, price)
+report("buy", "2020-03-13", 47.149, 7000, price)
+report("buy", "2020-02-26", 47.794, 10000, price)
 
 url = "https://www.onvista.de/fonds/UNIRAK-EUR-DIS-Fonds-DE0008491044"
 price, change = fetch (url, "span", 'price', "span", 'performance-pct')
 print("%s %8.2f EUR %s" % ("UniRak:".ljust(15), price, change))
-report("2018-06-29", 82.432, 10000, price)
-report("2018-02-14", 85.657, 10000, price)
+report("buy", "2018-06-29", 82.432, 10000, price)
+report("buy", "2018-02-14", 85.657, 10000, price)
 
 
