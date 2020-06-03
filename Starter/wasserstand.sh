3162c52dc639472ccecebc7d3dec6858c89e45e5
Nicolas <github@site7even.de>
2020-06-03 19:25:15 +0200

    added orientation of starter

--- Starter/wasserstand.sh
+++ Starter/wasserstand.sh
@@ -1,1 +1,2 @@
-python3 -u ~/dev/private/python-tools/scrape.py | zenity --text-info --width=1000 --height=300 --font=courier --title=Wasserstand
+cd `dirname "$(readlink -f "$0")"`
+python3 -u ../scrape.py | zenity --text-info --width=1000 --height=300 --font=courier --title=Wasserstand
